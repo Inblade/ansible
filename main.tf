aa4fda4319d6308879a6cc6301bd8baa06681cf4
Danylo <dkocheto@gmail.com>
2021-03-18 20:56:01 +0000

    Update main.tf, output.tf, playbook.yml, provider.tf, terraform.tfvars.sample, variables.tf files

---
+++ main.tf
@@ -0,0 +1,32 @@
+#Data block
+data "digitalocean_ssh_key" "rebrain" {
+  name = "REBRAIN.SSH.PUB.KEY"
+}
+#create resource my_ssh_key
+resource "digitalocean_ssh_key" "my_ssh_devops" {
+  name       = "my_ssh_devops"
+  public_key = file("/home/inblade/.ssh/id_rsa.pub")
+}
+#create tags for DO droplet
+resource "digitalocean_tag" "devops" {
+  name = "devops"
+}
+resource "digitalocean_tag" "mail" {
+  name = "dkocheto_at_gmail_com"
+}
+#create DO droplet
+resource "digitalocean_droplet" "Dkocheto" {
+  image    = "ubuntu-20-04-x64"
+  name     = "Dkocheto"
+  region   = "nyc3"
+  size     = "s-1vcpu-1gb"
+  tags     = [digitalocean_tag.devops.id, digitalocean_tag.mail.id]
+  ssh_keys = [digitalocean_ssh_key.my_ssh_devops.id, data.digitalocean_ssh_key.rebrain.id]
+}
+
+resource "local_file" "hosts" {
+  filename = "hosts"
+  content = templatefile("${path.module}/inventory.tpl", {
+    ip = digitalocean_droplet.Dkocheto.*.ipv4_address
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "do_token" {
+  type        = string
+  description = "digitalocean_token"
+}
+variable "my_ssh_devops" {
+  type        = string
+  description = "my_ssh_key"
+}
+variable "rebrain" {
+  type        = string
+  description = "rebrain_ssh_key"
+}
+variable "ssh_fingerprint" {
+  type        = string
+  description = "fingerprint"
+}
+variable "my_ssh_devops1" {
+  type = string
+}
