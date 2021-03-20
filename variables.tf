de32c1be3d77771eb4cece0ca0faf5ed1117d14c
Danylo <dkocheto@gmail.com>
2021-03-20 22:21:57 +0000

    Update variables.tf Deleted templates/nginx.conf.j2, .gitignore, inventory.tpl, main.tf, output.tf, playbook.yml, provider.tf files

--- variables.tf
+++ variables.tf
@@ -4,12 +4,8 @@ variable "do_token" {
 }
 variable "my_ssh_devops" {
   type        = string
-  description = "#my_ssh_key"
+  description = "my_ssh_key"
 }
-/*variable "my_ssh_privetops" {
-  type        = string
-  description = "#my_ssh_privetops"
-}*/
 variable "rebrain" {
   type        = string
   description = "rebrain_ssh_key"
@@ -18,12 +14,6 @@ variable "ssh_fingerprint" {
   type        = string
   description = "fingerprint"
 }
-/*variable "conumb" {
-  type        = list(any)
-  default     = ["1"]
-  description = "var array"
+variable "my_ssh_devops1" {
+  type = string
 }
-variable "devs" {
-  type    = list(string)
-  default = ["ansible-Dkocheto"]
-}*/
