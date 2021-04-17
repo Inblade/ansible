1b74d7eb5c5ecfa98ae6905ff8e68a6b88525bc9
Danylo <dkocheto@gmail.com>
2021-04-17 10:16:20 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "do_token" {
   type        = string
   description = "digitalocean_token"
 }
-variable "my_ssh_dkochetov1" {
+variable "my_ssh_dkocheto" {
   type        = string
   description = "my_ssh_key"
 }
@@ -14,6 +14,9 @@ variable "ssh_fingerprint" {
   type        = string
   description = "fingerprint"
 }
+variable "my_ssh_dkocheto1" {
+  type = string
+}
 variable "aws_access_key" {
   type        = string
   description = "aws_access_key"
@@ -22,19 +25,13 @@ variable "aws_secret_key" {
   type        = string
   description = "aws_secret_key"
 }
-
-#variable "zone_id" {
-#type=string
-#}
-
-variable "domains" {
-  type = list(any)
-}
-
 variable "connection" {
   default = {
     user        = "root"
     type        = "ssh"
     private_key = "/home/inblade/.ssh/id_rsa"
   }
+}
+variable "domains" {
+  type = list(any)
 }
